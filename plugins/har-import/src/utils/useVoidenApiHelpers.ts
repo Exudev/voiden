895e4323116fc5d0810ee421b55141f21e18e403
Pablo Díaz <pablo.euardo10@gmail.com>
2026-08-06 14:20:45 -0400

    feat(plugin): add HTTP Archive (HAR) file importer plugin

---
+++ plugins/har-import/src/utils/useVoidenApiHelpers.ts
@@ -0,0 +1,32 @@
+/**
+ * Hook to access voiden-api plugin helpers
+ */
+
+export interface VoidenApiHelpers {
+  createMethodNode: (method: string) => any;
+  createUrlNode: (url: string) => any;
+  createHeadersTableNode: (headers: [string, string][]) => any;
+  createJsonBodyNode: (body: string, contentType: string) => any;
+  createXMLBodyNode: (body: string, contentType: string) => any;
+  createMultipartTableNode: (formData: [string, string][]) => any;
+  createUrlTableNode: (formData: [string, string][]) => any;
+  createQueryTableNode: (params: [string, string][]) => any;
+  convertToVoidMarkdown: (jsonContent: any) => Promise<string>;
+  convertBlocksToVoidFile: (title: string, blocks: any[]) => string;
+  insertParagraphAfterRequestBlocks: (content: any[]) => any[];
+}
+
+/**
+ * Get voiden-api helpers from the global window object
+ */
+export function getVoidenApiHelpers(): VoidenApiHelpers {
+  const helpers = (window as any).__voidenHelpers__?.['voiden-wrapper-api-extension'];
+
+  if (!helpers) {
+    throw new Error(
+      'Voiden API helpers not found. Make sure voiden-wrapper-api-extension is loaded before har-import.'
+    );
+  }
+
+  return helpers;
+}
